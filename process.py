0091bb050fdedf566b2b0e051004f6524cf7a405
QuangHari <113539125+QuangHari@users.noreply.github.com>
2023-05-07 00:21:49 +0700

    loadSquare

--- process.py
+++ process.py
@@ -3,33 +3,78 @@ import numpy as np
 import os
 from utils import *
 from GradedAnswerSheet import *
+import imutils
 
-height = 559
-width = 393
+height = 840
+width = 615
+
 
 def preprocess(img):
     img = cv2.resize(img, (width, height))
 
     imgContours = img.copy()
     imgRectCon = img.copy()
+    imgSelectedCon = img.copy()
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     imgBlur = cv2.GaussianBlur(imgGray, (3, 3), 1)
-    imgCanny = cv2.Canny(imgBlur, 10, 50)
+    imgCanny = cv2.Canny(imgBlur, 20, 50)
 
     # Finding all contours
-    contours, hierarchy = cv2.findContours(imgCanny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    contours, hierarchy = cv2.findContours(imgCanny, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     imgContours = cv2.drawContours(imgContours, contours, -1, (0, 255, 0)[::-1], 1)
 
     # Find rects
     rectCon = rect_contour(contours)
-    imgRectCon = cv2.drawContours(imgRectCon, rectCon, -1, (0, 255, 0)[::-1], 1)
+    contour1 = get_corner_points(rectCon[7])
+    # contour2 = get_corner_points(rectCon[1])
+    # contour3 = get_corner_points(rectCon[2])
+    # contour4 = get_corner_points(rectCon[3])
+
+
+    cv2.drawContours(imgSelectedCon, contour1, -1, (0, 255, 0)[::-1], 10)
+    # cv2.drawContours(imgSelectedCon, contour2, -1, (0, 0, 255)[::-1], 10)
+    # cv2.drawContours(imgSelectedCon, contour3, -1, (255, 0, 0)[::-1], 10)
+    # cv2.drawContours(imgSelectedCon, contour4, -1, (0, 255, 255)[::-1], 10)
+    # cv2.drawContours(imgSelectedCon, rectCon, -1, (0, 255, 255)[::-1], 1)
+
+    biggestContour = reorder(contour1)
+
+    pt1 = np.float32(biggestContour)
+    pt2 = np.float32([[0, 0], [width, 0], [0, height], [width, height]])
+    matrix = cv2.getPerspectiveTransform(pt1, pt2)
+    imgWarpColored = cv2.warpPerspective(img, matrix, (width, height))
+
+    # Threshold
+    imgWarpgray = cv2.cvtColor(imgWarpColored, cv2.COLOR_BGR2GRAY)
+    imgThresh = cv2.threshold(imgWarpgray, 150, 255, cv2.THRESH_BINARY_INV)[1]
+
+    # splitBoxes(imgWarpColored)
+    thresh = cv2.threshold(imgWarpgray, 0, 255,
+                           cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+                            cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    questionCnts = []
+    for c in cnts:
+        # compute the bounding box of the contour, then use the
+        # bounding box to derive the aspect ratio
+        (x, y, w, h) = cv2.boundingRect(c)
+        ar = w / float(h)
+        # in order to label the contour as a question, region
+        # should be sufficiently wide, sufficiently tall, and
+        # have an aspect ratio approximately equal to 1
+        if w >= 30 and h >= 30 and h<100 and ar >= 0.7 and ar <= 1.3:
+            questionCnts.append(c)
+        img3 = cv2.drawContours(imgWarpColored, questionCnts, -1, (0, 255, 0)[::-1], 3)
+
 
     # imgArray = [img, imgGray, imgBlur, imgCanny, imgContours]
     # imgArray = [img, imgContours]
-    imgArray = [imgContours, imgRectCon]
-    imgStack = stack_images(1.4, imgArray)
+    imgArray = [imgContours, imgSelectedCon, imgWarpColored]
+    imgStack = stack_images(0.5, imgArray)
     cv2.imshow("stacked image", imgStack)
     cv2.waitKey(0)
+
 
 def process(img, gradedAnswerSheets):
     preprocess(img)
--- utils.py
+++ utils.py
@@ -54,12 +54,12 @@ def rect_contour(contours):
         # print("Area ", area)
 
         # Filter by area
-        if area > 100:
+        if area > 1000:
             peri = cv2.arcLength(contour, True)
-            approx = cv2.approxPolyDP(contour, 0.02 * peri, True)
-            # print("Area", area, ", Corner points", len(approx))
+            approx = cv2.approxPolyDP(contour, 0.01 * peri, True)
+            #print("Area", area, ", Corner points", len(approx))
 
-            if len(approx) == 4:
+            if len(approx) ==4 :
                 rectCon.append(contour)
 
     # sort descending rectangle contours by area
@@ -68,6 +68,50 @@ def rect_contour(contours):
     return rectCon
 
 def get_corner_points(contour):
+    # contour.get
+
     peri = cv2.arcLength(contour, True)
-    approx = cv2.approxPolyDP(contour, 0.02 * peri, True)
+    approx = cv2.approxPolyDP(contour, 0.01 * peri, True)
     return approx
+
+
+
+def reorder(myPoints):
+    myPoints = myPoints.reshape((4,2))
+    myPointNew= np.zeros((4,1,2),np.int32)
+    add = myPoints.sum(1)
+    # print(myPoints)
+    # print(add)
+    myPointNew[0]  = myPoints[np.argmin(add)] # [0,0]
+    myPointNew[3] = myPoints[np.argmax(add)]   #[w,h]
+    diff = np.diff(myPoints,axis=1)
+    myPointNew[1] = myPoints[np.argmin(diff)]  # [w,0]
+    myPointNew[2] = myPoints[np.argmax(diff)]  # [h,0]
+
+
+    return myPointNew
+
+def numberDetection(img):
+    imgContours = img.copy()
+    imgGray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    imgBlur = cv2.GaussianBlur(imgGray, (3, 3), 1)
+    imgCanny = cv2.Canny(imgBlur, 20, 35)
+
+    contours, hierarchy = cv2.findContours(imgCanny, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    imgContours = cv2.drawContours(imgContours, contours, -1, (0, 255, 0)[::-1], 1)
+
+    kernel = np.ones((5, 5), np.uint8)
+
+    # Perform dilation on the image
+    dilated = cv2.dilate(imgCanny, kernel, iterations=1)
+    cv2.imshow("A",dilated)
+
+def splitBoxes(img):
+    a =1
+    # row = np.vsplit(img,6)
+    # col =np.hsplit(row[4],5)
+    # cv2.imshow("im1", row[1])
+    # cv2.imshow("im2", col[2])
+    # cv2.imshow("im3", col[3])
+    # cv2.imshow("im4", col[4])
+
